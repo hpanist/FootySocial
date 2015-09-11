7241c088189b48c2eb33256fd6bfc9289a9786b4
Dipo Isola <isolaoluwatosin@gmail.com>
2015-09-11 15:55:55 +0100

    add bootsnipp effects

--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -1,46 +1,59 @@
-<nav class="navbar navbar-inverse">
-    <div class="container-fluid">
-      <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#">FootySocial</a>
-      </div>
+<div class="navbar-wrapper">
+	<div class="container-fluid">
+			<nav class="navbar navbar-fixed-top">
+				<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Logo</a>
 
-      <!-- Collect the nav links, forms, and other content for toggling -->
-      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav">
-          <li class="active"><a href="#">Today <span class="sr-only">(current)</span></a></li>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Predict <span class="caret"></span></a>
-            <ul class="dropdown-menu" role="menu">
-              <li><a href="#">Today's Prediction</a></li>
-              <li class="divider"></li>
-              <li><a href="#">Top Matches</a></li>
-            </ul>
-          </li>
-        </ul>
-        <form class="navbar-form navbar-left" role="search">
-          <div class="form-group">
-            <input type="text" class="form-control" placeholder="Search">
-          </div>
-          <button type="submit" class="btn btn-default">Submit</button>
-        </form>
-        <ul class="nav navbar-nav navbar-right">
-          <li><a href="#">Hot Topics</a></li>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Enter <span class="caret"></span></a>
-            <ul class="dropdown-menu" role="menu">
-              <li><a href="#">Login</a></li>
-              <li class="divider"></li>
-              <li><a href="#">Join the Fun</a></li>
-            </ul>
-          </li>
-        </ul>
-      </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-</nav>
+					</div>
+					<div id="navbar" class="navbar-collapse collapse">
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="#" class="">Home</a></li>
+
+								<li class=" dropdown"><a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Departments <span class="caret"></span></a>
+									<ul class="dropdown-menu">
+											<li><a href="#">View Departments</a></li>
+											<li><a href="#">Add New</a></li>
+									</ul>
+								</li>
+								<li class=" dropdown"><a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Managers <span class="caret"></span></a>
+									<ul class="dropdown-menu">
+											<li><a href="#">View Managers</a></li>
+											<li><a href="#">Add New</a></li>
+									</ul>
+								</li>
+								<li class=" dropdown"><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Staff <span class="caret"></span></a>
+									<ul class="dropdown-menu">
+											<li><a href="#">View Staff</a></li>
+											<li><a href="#">Add New</a></li>
+											<li><a href="#">Bulk Upload</a></li>
+									</ul>
+								</li>
+								<li class=" down"><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">HR <span class="caret"></span></a>
+									<ul class="dropdown-menu">
+											<li><a href="#">Change Time Entry</a></li>
+											<li><a href="#">Report</a></li>
+									</ul>
+								</li>
+
+						</ul>
+						<ul class="nav navbar-nav pull-right">
+							<li class=" dropdown"><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Signed in as  <span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="#">Change Password</a></li>
+									<li><a href="#">My Profile</a></li>
+								</ul>
+							</li>
+							<li class=""><a href="#">Logout</a></li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+	</div>
+</div>
