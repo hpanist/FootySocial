520e1745155f3b573cb8621a66d7ed11b8451121
Dipo Isola <isolaoluwatosin@gmail.com>
2015-09-21 14:46:59 +0100

    Trying squash out
    Make css changes
    
    Add css folder
    
    Clean up view pages

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,15 +1,16 @@
 <html>
-<head>
-  <title> @yield('title') </title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-</head>
-<body>
+	<head>
+	  <title> @yield('title') </title>
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	</head>
 
-@include('shared.navbar')
+	<body>
 
-@yield('content')
+		@include('shared.navbar')
 
-</body>
+		@yield('content')
+
+	</body>
 </html>
