3fd83540a2a56a9c143ef0b1c92b853a24a05067
Dipo Isola <isolaoluwatosin@gmail.com>
2015-10-02 10:53:43 +0100

    Include profile in routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,6 +13,7 @@
 
 Route::get('/', 'PagesController@home');
 Route::get('enter', 'PagesController@enter');
+Route::get('profile', 'PagesController@profile');
 
 Route::get('users/register', 'Auth\AuthController@getRegister');
 Route::post('users/register', 'Auth\AuthController@postRegister');
