565bdf71bf5c192368006725430fa0edeeb27149
Oladipupo Isola <isolaoluwatosin@gmail.com>
2015-10-14 09:56:09 +0100

    Make further namespace changes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -28,17 +28,17 @@ $app = new Illuminate\Foundation\Application(
 
 $app->singleton(
     Illuminate\Contracts\Http\Kernel::class,
-    App\Http\Kernel::class
+    FootySocial\Http\Kernel::class
 );
 
 $app->singleton(
     Illuminate\Contracts\Console\Kernel::class,
-    App\Console\Kernel::class
+    FootySocial\Console\Kernel::class
 );
 
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
-    App\Exceptions\Handler::class
+    FootySocial\Exceptions\Handler::class
 );
 
 /*
