4463897c62e7e065ac5750da6e5bec85fffd2db9
Oladipupo Isola <isolaoluwatosin@gmail.com>
2015-10-19 22:17:30 +0100

    Solve User registration issues

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -12,80 +12,95 @@ use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
 class AuthController extends Controller
 {
-  /*
-  |--------------------------------------------------------------------------
-  | Registration & Login Controller
-  |--------------------------------------------------------------------------
-  |
-  | This controller handles the registration of new users, as well as the
-  | authentication of existing users. By default, this controller uses
-  | a simple trait to add these behaviors. Why don't you explore it?
-  |
-  */
+	/*
+	|--------------------------------------------------------------------------
+	| Registration & Login Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller handles the registration of new users, as well as the
+	| authentication of existing users. By default, this controller uses
+	| a simple trait to add these behaviors. Why don't you explore it?
+	|
+	*/
 
-  use AuthenticatesAndRegistersUsers, ThrottlesLogins;
+	use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
-  protected $redirectPath = '/profile';
-  protected $registerPath = '/register';
-  protected $loginPath = '/';
+	protected $redirectPath = '/profile';
+	protected $registerPath = 'auth/register';
+	protected $loginPath = '/auth/login';
 
 
-  /**
-   * Create a new authentication controller instance.
-   *
-   * @return void
-   */
-  public function __construct()
+	/**
+	 * Create a new authentication controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$this->middleware('guest', ['except' => 'getLogout']);
+	}
+
+	/**
+	 * Get a validator for an incoming registration request.
+	 *
+	 * @param  array  $data
+	 * @return \Illuminate\Contracts\Validation\Validator
+	 */
+	protected function validator(array $data)
+	{
+		return Validator::make($data, [
+			'username' => 'required|max:255',
+			'email' => 'required|email|max:255|unique:users',
+			'password' => 'required|confirmed|min:5',
+		]);
+	}
+
+	/**
+	 * Create a new user instance after a valid registration.
+	 *
+	 * @param  array  $data
+	 * @return User
+	 */
+	protected function create(array $data)
+	{
+		return User::create([
+			'email' => $data['email'],
+			'username' => $data['username'],
+			'password' => bcrypt($data['password']),
+		]);
+	}
+
+	public function getRegister()
+	{
+		return view('auth.register');
+	}
+
+	public function postLogin(Request $request)
   {
-    $this->middleware('guest', ['except' => 'getLogout']);
+      $this->validate($request, [
+          'email' => 'required',
+          'password' => 'required'
+      ]);
+
+      $authStatus = Auth::attempt($request->only(['email', 'password']), $request->has('remember'));
+      if (!$authStatus) {
+          return redirect()->back()->with('info', 'Invalid Email or Password');
+      }
+
+      return redirect()->route('index')->with('info', 'You are now signed in');
   }
 
-  /**
-   * Get a validator for an incoming registration request.
-   *
-   * @param  array  $data
-   * @return \Illuminate\Contracts\Validation\Validator
-   */
-  protected function validator(array $data)
-  {
-    return Validator::make($data, [
-      'username' => 'required|max:255',
-      'email' => 'required|email|max:255|unique:users',
-      'password' => 'required|confirmed|min:5',
-    ]);
-  }
-
-  /**
-   * Create a new user instance after a valid registration.
-   *
-   * @param  array  $data
-   * @return User
-   */
-  protected function create(array $data)
-  {
-    return User::create([
-      'email' => $data['email'],
-      'username' => $data['username'],
-      'password' => bcrypt($data['password']),
-    ]);
-  }
-
-  public function getRegister()
-  {
-    return view('auth.register');
-  }
-
-  // public function postRegister(Request $request)
-  // {
-  //   return Response::make('Registration successful!');
-  //   $validator = $this->validator($request->all());
-  //
-  //   if ($validator->fails())
-  //   {
-  //     $this->throwValidationException($request, $validator);
-  //   }
-  //
-  //   $this->auth->login($this->registrar->create($request->all()));
-  //   return redirect($this->redirectPath());
-  // }
+	// public function postRegister(Request $request)
+	// {
+	//   return Response::make('Registration successful!');
+	//   $validator = $this->validator($request->all());
+	//
+	//   if ($validator->fails())
+	//   {
+	//     $this->throwValidationException($request, $validator);
+	//   }
+	//
+	//   $this->auth->login($this->registrar->create($request->all()));
+	//   return redirect($this->redirectPath());
+	// }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,21 +8,21 @@
       <div class="panel panel-default">
         <div class="panel-body">
           <h5 class="text-center"><b>Enter!</b></h5>
-            <form class="form form-signup" role="form">
+            <form method="POST" action="auth/login" class="form form-signup" role="form">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
                 <div class="form-group">
                   <div class="input-group">
                     <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
-                    <input type="text" class="form-control" placeholder="Username" />
+                    <input type="text" class="form-control" name="username" placeholder="Username" />
                   </div>
                 </div>
                 <div class="form-group">
                   <div class="input-group">
                     <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
-                    <input type="password" class="form-control" placeholder="Password" />
+                    <input type="password" class="form-control" name="password" placeholder="Password" />
                   </div>
                 </div>
-            <button type="submit" class="btn btn-sm btn-success btn-block" role="button">Submit</button>
+            	<button type="submit" class="btn btn-sm btn-success btn-block" role="button">Submit</button>
           </form>
         </div>
       </div>
