fef6292d64b3377d52895314ae6d550bfa27501c
Oladipupo Isola <isolaoluwatosin@gmail.com>
2015-10-22 22:25:59 +0100

    Add base work on FS timeline

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,10 @@
 	<div class="container">
 	  <div class="content">
 	    <div class="title garamondize">Home </div>
+	    <div style="width: 50%; display: block; margin: 0 auto;">
+		    <span class="garamondize" style="text-align: center;">Update your status</span>
+		    <textarea class="form-control" rows="3"></textarea>
+	    </div>
 	  </div>
 	</div>
 @endsection
