5b2467c3cadc85098bcba77682832b347c5d3c7e
Oladipupo Isola <isolaoluwatosin@gmail.com>
2015-10-25 23:25:53 +0100

    YEAH!

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
     <div class="col-md-4 col-md-offset-4">
       <div class="panel panel-default">
         <div class="panel-body">
-          <h5 class="text-center"><b>Enter!</b></h5>
+          <h5 class="text-center garamondize"><b>Enter!</b></h5>
 
           	@if (count($errors) > 0)
 	            <div class="alert alert-danger">
