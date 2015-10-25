b6ddec6c244c71020996354b2f5c83e0dcafbd67
Oladipupo Isola <isolaoluwatosin@gmail.com>
2015-10-25 22:55:07 +0100

    Redesign reset password page

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -1,43 +1,44 @@
 @extends('layout.master')
-@section('title', 'Reset password')
+@section('title', 'Reset Password')
 
 @section('content')
-	<h4 align="center" class="garamondize">Provide your e-mail address to reset your password.</h4>
-	<div class="container">
-			@if (count($errors) > 0)
-				<div class="alert alert-danger">
-					<strong>Whoops!</strong><br>
-					<ul>
-						@foreach ($errors->all() as $error)
-							<li>{{ $error }}</li>
-						@endforeach
-					</ul>
-				</div>
-			@endif
+<div class="container">
+	<div class="row">
+		<div class="col-md-4 col-md-offset-4">
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<h5 class="text-center garamondize"><b>Enter your email to reset password</b></h5>
 
-			@if(session('status'))
-				<div class="alert alert-success">
-					{{ session('status') }}
-				</div>
-			@endif
-			<form method="POST" action="/password/email" class="form-inline" style="text-align: center">
-				<input type="hidden" name="_token" value="{{ csrf_token() }}">
-				<div class="form-group">
-				  <label class="garamondize" for="exampleInputEmail2">Email</label>
-				  <input type="email" name="email" class="form-control" id="exampleInputEmail3" placeholder="">
-				</div>
-				<button type="submit" class="btn btn-success">Reset </button>
-			</form>
+						@if (count($errors) > 0)
+							<div class="alert alert-danger">
+									<strong>Whoops!</strong> There were some problems with your input.<br><br>
+									<ul>
+											@foreach ($errors->all() as $error)
+													<li>{{ $error }}</li>
+											@endforeach
+									</ul>
+							</div>
+						@endif
 
-			<!-- <form method="POST" action="/password/email">
-				<input type="hidden" name="_token" value="{{ csrf_token() }}">
-				<div class="">
-				  <input class="" placeholder="E-mail address" name="email" type="email" required>
-				</div>
+						@if(session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            @endif
 
-				<div align="center" class="mui-form-group">
-				  <button class="" name="reset" type="submit">Reset</button>
+						<form method="POST" action="/password/email" class="form form-signup" role="form">
+								<input type="hidden" name="_token" value="{{ csrf_token() }}">
+								<div class="form-group">
+									<div class="input-group">
+										<span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+										<input type="email" class="form-control" name="email" placeholder="Email" />
+									</div>
+								</div>
+							<button type="submit" class="btn btn-sm btn-success btn-block" role="button">Reset password</button>
+					</form>
 				</div>
-			</form> -->
+			</div>
+		</div>
 	</div>
+</div>
 @endsection
